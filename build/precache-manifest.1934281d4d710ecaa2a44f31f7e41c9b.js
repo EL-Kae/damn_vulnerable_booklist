726acef15d557d9e47640b30128153b8bf7431d8
EL-Kae <KaestnerLeonardo@gmail.com>
2023-12-25 21:36:31 -0500

    Add files via upload

---
+++ build/precache-manifest.1934281d4d710ecaa2a44f31f7e41c9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbeca610036844853098633bd039d019",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05b04da113361122d11e",
+    "url": "/static/css/main.1f741b2e.chunk.css"
+  },
+  {
+    "revision": "dbc119391204cf30a52c",
+    "url": "/static/js/2.55a886c5.chunk.js"
+  },
+  {
+    "revision": "05b04da113361122d11e",
+    "url": "/static/js/main.dd65cec4.chunk.js"
+  },
+  {
+    "revision": "6a64615d683a58384859",
+    "url": "/static/js/runtime~main.c5ef3462.js"
+  }
+]);
